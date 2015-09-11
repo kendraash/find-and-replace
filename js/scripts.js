b65e80bae7fceefe6cdddc410c403cf2fd63d9c7
Kendra Ash <kendra.ash32@gmail.com>
2015-09-11 08:34:19 -0700

    returns the new sentence with the replaceWord

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 var findAndReplace = function(sentence, findWord, replaceWord) {
   if(sentence && findWord && replaceWord != null && (findWord != replaceWord) )
   {
-    return true;
+    // sentence = sentence.LowerCase();
+    // var position = sentence.search(/findWord/i);
+    var final_sentence = sentence.replace(findWord, replaceWord)
+    return final_sentence;
   }
   else {
     return false;
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,9 @@ describe("findAndReplace", function() {
   it("returns false if the findWord and replaceWord are the same", function() {
     expect(findAndReplace("Good Morning", "morning", "morning")).to.equal(false)
   });
+
+  it("returns the new sentence with the replaceWord", function() {
+    expect(findAndReplace("Good morning", "morning", "evening")).to.equal("Good evening")
+  });
+
 });
