444db0098dcdd0d4d5a0182687a2eefafa018a69
Kendra Ash <kendra.ash32@gmail.com>
2015-09-11 08:52:16 -0700

    add jquery for web browser

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,21 @@ var findAndReplace = function(sentence, findWord, replaceWord) {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#find-and-replace").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var findWord = $("input#findWord").val();
+    var replaceWord = $("input#replaceWord").val();
+
+    var output = findAndReplace(sentence, findWord, replaceWord);
+
+    $(".originalSentence").text(sentence);
+    $(".findWord").text(findWord);
+    $(".replaceWord").text(replaceWord);
+    $(".output").text(output);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
